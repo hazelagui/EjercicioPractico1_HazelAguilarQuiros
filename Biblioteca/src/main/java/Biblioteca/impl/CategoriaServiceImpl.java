3a2d1f7e547caf6c40ba26ee2a3b44283980bf2c
hazelagui <hazelaguilarquiros@gmail.com>
2025-10-22 20:50:07 -0600

    Correcciones

--- Biblioteca/src/main/java/Biblioteca/impl/CategoriaServiceImpl.java
+++ Biblioteca/src/main/java/Biblioteca/impl/CategoriaServiceImpl.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+package Biblioteca.impl;
 import Biblioteca.domain.Categoria;
 import Biblioteca.repository.CategoriaRepository;
 import Biblioteca.service.CategoriaService;
